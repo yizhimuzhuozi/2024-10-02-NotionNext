ed89fbe509e504fc2c16cf79e1b936367ba95235
阿昆Time <31234254+yizhimuzhuozi@users.noreply.github.com>
2025-04-01 10:50:38 +0800

    Update font.config.js

--- conf/font.config.js
+++ conf/font.config.js
@@ -16,7 +16,7 @@ module.exports = {
   ],
   // 无衬线字体 例如'"LXGW WenKai"'
   FONT_SANS: [
-    // '"LXGW WenKai"',
+    '"LXGW WenKai"',
     '"PingFang SC"',
     '-apple-system',
     'BlinkMacSystemFont',
@@ -36,7 +36,7 @@ module.exports = {
   ],
   // 衬线字体 例如'"LXGW WenKai"'
   FONT_SERIF: [
-    // '"LXGW WenKai"',
+    '"LXGW WenKai"',
     'Bitter',
     '"Noto Serif SC"',
     'SimSun',
